2cdece91ab5022e412a058b944df279e8a101ac9
Kunj Bhatia <kunj.bhatia@w3villa.com>
2025-12-23 12:23:40 +0530

    minor refinements to footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ import { faG } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   return (
     <footer className="mt-8 bg-card text-white">
-      <div className="grid grid-cols-1 gap-10 px-8 py-10 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-10 px-8 py-8 md:grid-cols-3">
         <div className="flex flex-col gap-4">
           <div className="flex items-center gap-2">
             <div className="flex h-9 w-9 items-center justify-center rounded-md">
